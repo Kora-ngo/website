9b0efb4e35e9dfe284e4f154230f42a77ba0dfc9
Morel-D <morelDen@gmail.com>
2026-03-03 08:40:00 +0100

    google analytics attachment

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // App.tsx
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import './App.css';
 
 // Pages
@@ -7,10 +7,23 @@ import Home from './pages/home';
 import TermsOfService from './pages/terms';      // create this file next
 import PrivacyPolicy from './pages/privacy';     // create this file next
 import NotFound from './pages/not-found';        // create this file next
+import { useEffect } from 'react';
+import ReactGA from 'react-ga4'
+
+ReactGA.initialize("G-L5DS0KW5N9");
+
+function TrackPageViews() {
+  const location = useLocation();
+  useEffect(() => {
+    ReactGA.send({ hitType: 'pageview', page: location.pathname });
+  }, [location]);
+  return null;
+}
 
 function App() {
   return (
     <BrowserRouter>
+      <TrackPageViews />
       <Routes>
         {/* Main routes */}
         <Route path="/" element={<Home />} />
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import ReactGA from 'react-ga4'
+
+ReactGA.initialize("G-L5DS0KW5N9")
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
